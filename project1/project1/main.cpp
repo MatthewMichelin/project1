3995950d2c34f7847dfa1594f2b1c27aa38a5738
Matthew Michelin <matthew.michelin@gmail.com>
2025-04-30 23:25:17 -0400

    Xcode created.

---
+++ project1/project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by Matthew on 2025-04-30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
